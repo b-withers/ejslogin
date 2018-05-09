b4318367a0e94921524b52c0a33cd09170e0db8d
Brady <brady.withers@outlook.com>
2018-05-09 10:07:27 -0600

    working products

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,6 @@
 var router = require('express').Router();
+var User = require('../models/user');
+var Product = require('../models/product');
 
 
 
@@ -9,5 +11,19 @@ router.get('/', function(req, res){
 router.get('/about', function(req, res){
     res.render('main/about');
 });
+
+
+router.get('/products/:id', function(req, res, next){
+    Product
+    .find({ category: req.params.id })
+    .populate('catgeory')
+    .exec(function(err, products) {
+        if (err) return next (err);
+        res.render('main/category', {
+            products: products
+        });
+    });
+});
+
 
 module.exports = router;
